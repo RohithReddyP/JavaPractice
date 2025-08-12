6411cb0583270f2fcab3b2be1351ddf8296f5b96
Rohith Reddy <potharohithreddy@gmail.com>
2025-08-12 08:17:41 +0530

    {11/08/25}Committing Java DSA[basic(theory class from youtube)]

---
+++ DSA/dsaBasic.java
@@ -0,0 +1,21 @@
+package DSA;
+
+public class dsaBasic {
+    public static void main(String[] args) {
+        //sorted array
+        int arr[] = {12,22,34,45,65,70,99};
+
+        //access element in an array
+        System.out.println(arr[3]);     //for this time complexity is constant
+        // so time complexity is O(1)
+
+        //searching
+        //linear search
+        
+
+        //binary search
+
+
+
+    }
+}
