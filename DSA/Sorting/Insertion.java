8ed37f64a4b7a7c2bf6466537f94daba8d80f7c2
Rohith Reddy <potharohithreddy@gmail.com>
2025-08-24 15:52:02 +0530

    [23/8/25]Learned IS and QS

--- DSA/Sorting/Insertion.java
+++ DSA/Sorting/Insertion.java
@@ -4,6 +4,20 @@ public class Insertion {
     public static void main(String[] args) {
 
 
-        //Still need to do the Insertion Sort
+        int arr[] = {6,5,2,4,8,1,9,7};
+
+        for(int i=1;i<arr.length;i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while (j>=0 && arr[j] >key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] =key;
+        }
+        for (int num : arr){
+            System.out.print(num + " ");
+        }
     }
 }
---
+++ DSA/Sorting/Quick.java
@@ -0,0 +1,44 @@
+package DSA.Sorting;
+
+public class Quick {
+    public static void quickSort(int[] arr, int low, int high){
+        if (low<high){
+            int pivot = partition(arr, low, high);
+
+            quickSort(arr,low,pivot-1);
+            quickSort(arr,pivot+1,high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int piv = arr[high];
+        int i = low-1;
+
+        for (int j=low;j<high;j++){
+            if (arr[j]<piv){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {6,5,2,4,8,1,9,7};
+
+
+        quickSort(arr,0,arr.length-1);
+
+        for (int num : arr){
+            System.out.print(num + " ");
+        }
+
+    }
+}
